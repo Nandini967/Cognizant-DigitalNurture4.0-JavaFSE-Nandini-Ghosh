da989c0fd6be19b55a0e2669748ac131e4e401cd
Nandini Ghosh <155821223+Nandini967@users.noreply.github.com>
2025-08-10 00:45:11 +0530

    Add files via upload

---
+++ WEEK FOUR PART 2.2/spring-rest-handson-02/SpringCountryService/src/main/java/com/cognizant/springlearn/controller/CountryController.java
@@ -0,0 +1,19 @@
+package com.cognizant.springlearn.controller;
+
+import com.cognizant.springlearn.model.Country;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CountryController {
+
+    @RequestMapping("/SpringCountryService")
+    public Country getCountryIndia() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("country.xml");
+        Country country = (Country) context.getBean("in");
+        return country;
+    }
+}
